ba841f55f031981540caa014df879ee28da07494
garjones <gareth@gareth.com>
2024-12-12 23:04:25 -0800

    added paths to wget files

--- kiosk.install.sh
+++ kiosk.install.sh
@@ -19,16 +19,19 @@ sudo apt update
 sudo apt upgrade
 
 # install packages
-sudo apt install unclutter sed
+sudo apt install unclutter sed wget
 
 # move taskbar to bottom
 echo "position=bottom" >> .config/wf-panel-pi.ini
 
 # get kiosk files
-wget files from git
+wget https://raw.githubusercontent.com/garjones/pi-kiosk/main/kiosk.sh
+wget https://raw.githubusercontent.com/garjones/pi-kiosk/main/kiosk.run.sh
+wget https://raw.githubusercontent.com/garjones/pi-kiosk/main/kiosk.service
 
 # make kiosk.sh executable
-chmod u+x ~/kiosk.*
+chmod u+x ~/kiosk.sh
+chmod u+x ~/kiosk.run.sh
 
 # create startup service
 sudo ln -s /home/kcckiosk/kiosk.service /lib/systemd/system/kiosk.service
