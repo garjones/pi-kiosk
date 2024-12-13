487c6badb14723f40b8d72c03ad8b5ae046634a9
garjones <gareth@gareth.com>
2024-12-13 11:23:04 -0800

    Update kiosk.install.sh

--- kiosk.install.sh
+++ kiosk.install.sh
@@ -21,23 +21,27 @@ sudo apt upgrade
 # install packages
 sudo apt install unclutter sed wget
 
-# move taskbar to bottom
-echo "position=bottom" >> .config/wf-panel-pi.ini
-
 # get kiosk files
 wget https://raw.githubusercontent.com/garjones/pi-kiosk/main/kiosk.sh
 wget https://raw.githubusercontent.com/garjones/pi-kiosk/main/kiosk.run.sh
 wget https://raw.githubusercontent.com/garjones/pi-kiosk/main/kiosk.service
 
+# create startup service
+sudo ln -s /home/kcckiosk/kiosk.service /lib/systemd/system/kiosk.service
+
+# enable the kiosk service
+sudo systemctl enable kiosk.service
+
 # make kiosk.sh executable
 chmod u+x ~/kiosk.sh
 chmod u+x ~/kiosk.run.sh
 
-# create startup service
-sudo ln -s /home/kcckiosk/kiosk.service /lib/systemd/system/kiosk.service
+# move taskbar to bottom
+echo "position=bottom" >> .config/wf-panel-pi.ini
 
-# enable and start the service
-sudo systemctl enable kiosk.service
+# autorun the kiosk configuration on login
+echo "sudo /home/kcckiosk/kiosk.sh" >> .bashrc
 
 # we are done
 sudo reboot
+ 
