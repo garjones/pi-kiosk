64a01ef02a69c9aee06d842dac44c9bb374f30f4
garjones <gareth@gareth.com>
2024-12-13 13:07:05 -0800

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -12,7 +12,7 @@
 # 
 
 # autoupdate the service
-rm /home/kcckiosk/kiosk.run.sh
+sudo rm /home/kcckiosk/kiosk.run.sh
 wget https://raw.githubusercontent.com/garjones/pi-kiosk/main/kiosk.run.sh /home/kcckiosk/kiosk.run.sh
 
 # configuration parameters
