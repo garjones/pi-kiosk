c8caf2ce51073f2bf4a881be200f78ee304e44f7
garjones <gareth@gareth.com>
2025-01-19 23:02:37 -0800

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -73,7 +73,7 @@ KCC_ROTATION=${KCC_KIOSKCONFIG:3:1}
 
 # wlr-randr --output HDMI-A-1 --transform normal
 # wlr-randr --output HDMI-A-2 --transform normal
-
+XDG_RUNTIME_DIR=/run/user/1000
 wlr-randr --output HDMI-A-1 --transform 90
 wlr-randr --output HDMI-A-2 --transform 90
 
