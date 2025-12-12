40ed189c7b0e4323a7a2598ec21f55ef2f4e050a
Gareth Jones <gareth@gareth.com>
2025-12-12 14:24:58 -0800

    Added OQE entries (67)

--- kiosk.sh
+++ kiosk.sh
@@ -54,13 +54,17 @@ do_menu_main() {
 do_menu_cameras() {
   # display menu
   FUN=$(whiptail --title "$WT_TITLE" --backtitle "$WT_COPYRIGHT" --menu "Canera Options" $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT  --cancel-button Back --ok-button Select \
-  "C0102" "Cameras over sheets 1 & 2"     \
-  "C0304" "Cameras over sheets 3 & 4"     \
-  "C0506" "Cameras over sheets 5 & 6"     \
-  "C0708" "Cameras over sheets 7 & 8"     \
-  "C0910" "Cameras over sheets 9 & 10"    \
-  "C1112" "Cameras over sheets 11 & 12"   \
-  "A0000" "All Camera Test"               \
+  "C0102" "Cameras over sheets 1 & 2"       \
+  "C0304" "Cameras over sheets 3 & 4"       \
+  "C0506" "Cameras over sheets 5 & 6"       \
+  "C0708" "Cameras over sheets 7 & 8"       \
+  "C0910" "Cameras over sheets 9 & 10"      \
+  "C1112" "Cameras over sheets 11 & 12"     \
+  "-----" "───────────────────────────────" \
+  "C0304" "OQE - Cameras over sheets 3 & 4" \
+  "C0304" "OQE - Cameras over sheets 6 & 7" \
+  "-----" "───────────────────────────────" \
+  "A0000" "All Camera Test"                 \
   3>&1 1>&2 2>&3)
   RET=$?
 
